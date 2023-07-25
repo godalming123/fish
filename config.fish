b9dc3d05b8c87737be4ffdad323f8c14d420cc29
godalming123 <jamiesamglasson@gmail.com>
2023-07-25 15:11:27 +0100

    Update my fish config

--- config.fish
+++ config.fish
@@ -1,32 +1,44 @@
-# === FONT SETTINGS ===
-set reset (tput sgr0) & # styles
-set bold (tput bold) &
-
-set black (tput setaf 0) & # colors
-set red (tput setaf 1) &
-set green (tput setaf 2) &
-set yellow (tput setaf 3) &
-set blue (tput setaf 4) &
-set magenta (tput setaf 5) &
-set cyan (tput setaf 6) &
-set white (tput setaf 7) &
-
-# === IF USER IS USING PROMPT THEN SET EDITOR AND ALIAS ===
-if status is-interactive
-	set EDITOR nv
-	alias nv='nvim -u ~/Documents/neovim/init.lua'
-end
-
-# === FISH GREETING ===
 function fish_greeting
-    clear
+    if status is-interactive
+        set EDITOR hx
+        set -g fish_prompt_pwd_dir_length 5
+        set -g fish_prompt_pwd_full_length_dirs 5
+        if [ "$TERM" = "linux" ]
+            echo -en "\e]P0282c34" # Background color
+            echo -en "\e]PFFFFFFF" # Foreground color
+            echo -en "\e]P7abb2bf" # Alternate foreground color
+            echo -en "\e]P8abb2bf" # The foreground color used for fish suggested completions
+
+            # Normal color varients (should contrast with the background)
+            echo -en "\e]P1e06c75" # Red
+            echo -en "\e]P298c379" # Green
+            echo -en "\e]P3e5c07b" # Yellow
+            echo -en "\e]P461afef" # Blue
+            echo -en "\e]P5c678dd" # Magenta
+            echo -en "\e]P656b6c2" # Cyan
+
+            # Bright color varients (TODO: make these actually brighter then the normal colors)
+            echo -en "\e]P9e06c75" # Red
+            echo -en "\e]PA98c379" # Green
+            echo -en "\e]PBe5c07b" # Yellow
+            echo -en "\e]PC61afef" # Blue
+            echo -en "\e]PDc678dd" # Magenta
+            echo -en "\e]PEac4142" # Cyan
+
+            # Clear artifacts
+            clear
+        end
+    end
 end
 
-# === MY FISH PROMPT ===
 function fish_prompt
-	if test $status -eq 0
-		echo -n "$green > $reset"
-	else
-		echo -n "$red > $reset"
-	end
+    set -l _status $status
+    set_color blue
+    echo -n " "(pwd)
+    if test $_status -eq 0
+        set_color green
+    else
+        set_color red
+    end
+    echo -n " -> "
 end
